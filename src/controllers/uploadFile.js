7282e20afb5b4a22705e12533d597feae5521c60
kushaal-294-at-244994576460 <kushaal.varadan@acc.ltd>
2023-08-31 14:58:41 +0530

    Change in PNG Upload to S3

--- src/controllers/uploadFile.js
+++ src/controllers/uploadFile.js
@@ -2,11 +2,86 @@ const fs = require("fs");
 const path = require("path");
 const pdfHandler = require("./pdfHandler");
 const s3Utils = require("../utils/awsS3");
+const AWS = require("aws-sdk");
 
+// async function uploadPDF(req, res) {
+//   console.log("uploadPDF --> req --> ", req.file);
+//   try {
+//     console.log("dirname", __dirname);
+//     if (req.file) {
+//       const filePath = path.join(
+//         __dirname,
+//         `../../public/assets/${req.file.filename}`
+//       );
+//       const filename = req.file.filename.split(".")[0];
+//       const ext = req.file.filename.split(".")[1];
+//       console.log("Checking File Upload")
+//       if (fs.existsSync(filePath)) {
+//         // If PDF Uploaded to Server then convert to Png
+//         pdfHandler.pdfToImg(filename).then((result) => {
+//           console.log("result", result)
+//           if (result.error) {
+//             return result.status(400).json({
+//               status: 400,
+//               message: "Failed!",
+//               data: [],
+//               error: result.error,
+//             });
+//           } else {
+//             console.log("result", result);
+//             // Upload to S3 and get Presigned URL of 1st Page of PDF(png1)
+//             s3Utils
+//               .uploadToS3(result.data, filename)
+//               .then((uploadRes) => {
+//                 console.log("uploadRes", uploadRes)
+//                 return res.status(200).json({
+//                   status: 200,
+//                   message: uploadRes.message,
+//                   data: [],
+//                   count: result.data.length,
+//                 });
+//               })
+//               .catch((err) => {
+//                 return res.status(500).json({
+//                   status: 500,
+//                   message: "Failed!",
+//                   error: err.message,
+//                 });
+//               });
+//           }
+//         });
+//       }
+//     } else {
+//       return res.status(400).json({
+//         status: 400,
+//         message: "Failed!",
+//         data: [],
+//         error: "Please choose a PDF File",
+//       });
+//     }
+//   } catch (error) {
+//     return res.status(400).json({
+//       status: 500,
+//       message: "Failed!",
+//       data: [],
+//       error: error.message,
+//     });
+//   }
+// }
+
+//v2
 async function uploadPDF(req, res) {
   console.log("uploadPDF --> req --> ", req.file);
   try {
     console.log("dirname", __dirname);
+    const s3 = new AWS.S3({
+      endpoint: "s3-ap-south-1.amazonaws.com",
+      accessKeyId: "AKIAZDHWLVNJUUTAGIWY",
+      secretAccessKey: "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",
+      Bucket: "ivs-edu-book-pdf-v1",
+      signatureVersion: "v4",
+      region: "ap-south-1",
+    });
     if (req.file) {
       const filePath = path.join(
         __dirname,
@@ -14,9 +89,11 @@ async function uploadPDF(req, res) {
       );
       const filename = req.file.filename.split(".")[0];
       const ext = req.file.filename.split(".")[1];
+      console.log("Checking File Upload");
       if (fs.existsSync(filePath)) {
         // If PDF Uploaded to Server then convert to Png
-        pdfHandler.pdfToImg(filename).then((result) => {
+        pdfHandler.pdfToImg(filename).then(async (result) => {
+          console.log("result", result);
           if (result.error) {
             return result.status(400).json({
               status: 400,
@@ -25,16 +102,23 @@ async function uploadPDF(req, res) {
               error: result.error,
             });
           } else {
-            console.log("result", result);
             // Upload to S3 and get Presigned URL of 1st Page of PDF(png1)
             s3Utils
               .uploadToS3(result.data, filename)
-              .then((uploadRes) => {
-                return res.status(200).json({
-                  status: 200,
-                  message: uploadRes.message,
-                  data: [],
-                  count: result.data.length,
+              .then((data) => {
+                console.log("data", data);
+                if (data.length == result.data.length) {
+                  return res.status(200).json({
+                    status: 200,
+                    message: "All Files uploaded to S3",
+                    data: [],
+                    count: result.data.length,
+                  });
+                }
+                return res.status(500).json({
+                  status: 500,
+                  message: "All Files not Uploaded!",
+                  error: "All Files not Uploaded!",
                 });
               })
               .catch((err) => {
--- src/utils/awsS3.js
+++ src/utils/awsS3.js
@@ -34,64 +34,7 @@ function getSignedURL(filename) {
   }
 }
 
-function uploadToS3(dirname) {
-  try {
-    const s3 = new AWS.S3({
-      endpoint: "s3-ap-south-1.amazonaws.com",
-      accessKeyId: "AKIAZDHWLVNJUUTAGIWY",
-      secretAccessKey: "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",
-      Bucket: "ivs-edu-book-pdf-v1",
-      signatureVersion: "v4",
-      region: "ap-south-1",
-    });
-    // const noOfImgs = imgArray.length;
-    return new Promise((resolve, reject) => {
-      fs.readdir(assetPath + "/Images", (err, filenames) => {
-        const noOfImgs = filenames.length;
-        if (err) {
-          return reject({
-            error: err.message,
-            message: `Failed! Unable to upload to S3`,
-          });
-        }
-        filenames.forEach((fileName, i) => {
-          const buffer = fs.readFileSync(assetPath + "/Images/" + fileName);
-          console.log("buffer");
-          // console.log("buffer")
-          console.log("buffer");
-          const params = {
-            Bucket: "ivs-edu-book-pdf-v1",
-            Key: `${fileName}`,
-            Body: buffer,
-            // ContentType: "png",
-          };
-          s3.upload(params, (err, data) => {
-            if (err) {
-              console.log("err", err.message);
-              return reject({
-                error: err.message,
-                message: `Failed! Unable to upload to S3`,
-              });
-            }
-            if (i + i == noOfImgs) {
-              return resolve({
-                message: "All Pages uploaded to S3",
-                data: [data],
-              });
-            }
-          });
-        });
-      });
-    });
-  } catch (error) {
-    return {
-      message: "Failed! Unable to upload to S3",
-      error: error.message,
-      data: [],
-    };
-  }
-}
-// function uploadToS3(imgArray, filename) {
+// function uploadToS3(dirname) {
 //   try {
 //     const s3 = new AWS.S3({
 //       endpoint: "s3-ap-south-1.amazonaws.com",
@@ -101,38 +44,42 @@ function uploadToS3(dirname) {
 //       signatureVersion: "v4",
 //       region: "ap-south-1",
 //     });
-//     const noOfImgs = imgArray.length;
+//     // const noOfImgs = imgArray.length;
 //     return new Promise((resolve, reject) => {
-//       imgArray.map((img, i) => {
-//         const params = {
-//           Bucket: "ivs-edu-book-pdf-v1",
-//           Key: `${filename}_${i + 1}.png`,
-//           Body: img,
-//           ContentType: "png",
-//         };
-//         s3.upload(params, (err, data) => {
-//           if (err) {
-//             console.log("err", err.message);
-//             return reject({
-//               error: err.message,
-//               message: `Failed! Unable to upload ${i + 1} to S3`,
-//             });
-//           }
-//           if (i + i == noOfImgs) {
-//             return resolve({
-//               message: "All Pages uploaded to S3",
-//               data: [data],
-//             });
-//           }
-//           //
-//           // err
-//           //   ? reject({
-//           //       error: err.message,
-//           //       message: `Failed! Unable to upload ${i + 1} to S3`,
-//           //     })
-//           //   : i + i == noOfImgs
-//           //   ? resolve({ message: "All Pages uploaded to S3", data: [...data] })
-//           //   : null;
+//       fs.readdir(assetPath + "/Images", (err, filenames) => {
+//         const noOfImgs = filenames.length;
+//         if (err) {
+//           return reject({
+//             error: err.message,
+//             message: `Failed! Unable to upload to S3`,
+//           });
+//         }
+//         console.log("filenames", filenames)
+//         filenames.forEach((fileName, i) => {
+//           const buffer = fs.readFileSync(assetPath + "/Images/" + fileName);
+//           console.log("buffer");
+//           // console.log("buffer")
+//           const params = {
+//             Bucket: "ivs-edu-book-pdf-v1",
+//             Key: `${fileName}`,
+//             Body: buffer,
+//             // ContentType: "png",
+//           };
+//           s3.upload(params, (err, data) => {
+//             if (err) {
+//               console.log("err", err.message);
+//               return reject({
+//                 error: err.message,
+//                 message: `Failed! Unable to upload to S3`,
+//               });
+//             }
+//             if (i + i == noOfImgs) {
+//               return resolve({
+//                 message: "All Pages uploaded to S3",
+//                 data: [data],
+//               });
+//             }
+//           });
 //         });
 //       });
 //     });
@@ -144,5 +91,40 @@ function uploadToS3(dirname) {
 //     };
 //   }
 // }
+
+// v2
+function uploadToS3(dirname) {
+  try {
+    const s3 = new AWS.S3({
+      // endpoint: "s3-ap-south-1.amazonaws.com",
+      accessKeyId: "AKIAZDHWLVNJUUTAGIWY",
+      secretAccessKey: "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",
+      Bucket: "ivs-edu-book-pdf-v1",
+      signatureVersion: "v4",
+      region: "ap-south-1",
+    });
+    let promiseArr = [];
+    const filenames = fs.readdirSync(assetPath + "/Images");
+    console.log("filenames", filenames);
+    filenames.forEach(async (fileName, i) => {
+      const buffer = fs.readFileSync(assetPath + "/Images/" + fileName);
+      console.log("buffer");
+      const params = {
+        Bucket: "ivs-edu-book-pdf-v1",
+        Key: `${fileName}`,
+        Body: buffer,
+        // ContentType: "png",
+      };
+      promiseArr.push(s3.upload(params).promise());
+    });
+    return Promise.all(promiseArr);
+  } catch (error) {
+    return {
+      message: "Failed! Unable to upload to S3",
+      error: error.message,
+      data: [],
+    };
+  }
+}
 
 module.exports = { getSignedURL, uploadToS3 };
