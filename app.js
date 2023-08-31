726df1da18a143ecab8b6a7f740857b6e769ed65
kushaal-294-at-244994576460 <kushaal.varadan@acc.ltd>
2023-08-31 10:22:08 +0530

    App.js changes

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
+const http = require("http").Server(app);
 
 const signedURLRoute = require("./src/routes/signedS3Url");
 const uploadRoute = require("./src/routes/uploadPDF");
 
 const pdfToImg = require("./src/controllers/pdfHandler").pdfToImg;
 
-
 const app = express();
 const port = 3001;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,14 +18,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({ origin: "*" }));
 
 app.use("/", signedURLRoute);
-app.use("/", uploadRoute)
+app.use("/", uploadRoute);
 
 process.on("uncaughtException", function (err) {
   console.log(err);
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    Running: 1,
+  });
+});
 
-app.listen(port, (err) => {
+http.listen(port, (err) => {
   if (err) return console.log(`Server Issue due to ${err.message}`);
   return console.log(`Server listening on ${port}`);
 });
