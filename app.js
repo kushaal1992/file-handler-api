c980dc5cb4e747b88f6b30158e34251f1160d6da
kushaal-294-at-244994576460 <kushaal.varadan@acc.ltd>
2023-09-05 10:56:59 +0530

    Added magick command to Node{exec}

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-
 const signedURLRoute = require("./src/routes/signedS3Url");
 const uploadRoute = require("./src/routes/uploadPDF");
 const saveImageS3Route = require("./src/routes/saveImageS3");
 const pdfRoute = require("./src/routes/pdf.route");
+const { exec } = require("node:child_process");
 
 const pdfToImg = require("./src/controllers/pdfHandler").pdfToImg;
 
@@ -21,8 +21,8 @@ app.use(cors({ origin: "*" }));
 
 app.use("/", signedURLRoute);
 app.use("/", uploadRoute);
-app.use("/", saveImageS3Route)
-app.use("/", pdfRoute)
+app.use("/", saveImageS3Route);
+app.use("/", pdfRoute);
 
 process.on("uncaughtException", function (err) {
   console.log(err);
@@ -34,10 +34,20 @@ app.get("/", (req, res) => {
   });
 });
 
+exec(
+  `magick -density 150 /home/ubuntu/file-sample_150kB.pdf[0] -quality 100 -flatten -sharpen 0x1.0 file-sample_150kB_1_v1.png`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log("error", error);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  }
+);
+
 http.listen(port, (err) => {
   if (err) return console.log(`Server Issue due to ${err.message}`);
   return console.log(`Server listening on ${port}`);
 });
-
 
 module.exports = app;
